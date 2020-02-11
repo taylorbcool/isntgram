8d308d31cf5d1a8d6f3073cbd134a637480873e8
Taylor Cool <taycool@gmail.com>
2020-02-10 17:22:29 -0800

    created UI example

--- isntgram-client/src/App.js
+++ isntgram-client/src/App.js
@@ -8,7 +8,7 @@ import {
   Badge,
   Text
 } from "@chakra-ui/core";
-import { MdStar } from "react-icons/md";
+import { FaHeart } from "react-icons/fa";
 import { customTheme } from "./theme";
 
 function App() {
@@ -27,7 +27,7 @@ function App() {
       >
         <Image rounded="md" src="https://bit.ly/2k1H1t6" />
         <Flex align="baseline" mt={2}>
-          <Badge variantColor="brand">Plus</Badge>
+          <Badge variantColor="brand">Hot</Badge>
           <Text
             ml={2}
             textTransform="uppercase"
@@ -35,22 +35,22 @@ function App() {
             fontWeight="bold"
             color="brand.800"
           >
-            Verified &bull; Cape Town
+            Posted @ DateTime
           </Text>
         </Flex>
-        <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
+        {/* <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
           Modern, Chic Penthouse with Mountain, City & Sea Views
-        </Text>
-        <Text mt={2}>$119/night</Text>
+        </Text> */}
+        <Text mt={2}>username</Text>
         <Flex mt={2} align="center">
-          <Box as={MdStar} color="orange.400" />
+          <Box as={FaHeart} color="orange.400" />
           <Text ml={1} fontsize="sm">
-            <b>4.84</b> (190)
+            <b>484</b> (190)
           </Text>
         </Flex>
       </Box>
 
-      <Text textAlign="center">Create React App Chakra UI</Text>
+      <Text textAlign="center">UI Example</Text>
     </ThemeProvider>
   );
 }
