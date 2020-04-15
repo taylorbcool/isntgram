36ced0c0f7a1549c2406fd15072094b64a1da254
Kip DeCastro <kipdec@Kips-MacBook-Pro.local>
2020-04-15 16:30:29 -0600

    Removed dotnet project

--- src/service/isntservice/Models/CommentReturnModel.cs
+++
@@ -1,5 +0,0 @@
-namespace isntservice.Models {
-    public class CommentReturnModel : CommentModel {
-        public string UserName {get;set;}
-    }
-}
--- src/service/isntservice/runforteam.sh
+++
@@ -1,1 +0,0 @@
-dotnet run --urls http://0.0.0.0:5000 https://0.0.0.0:5001
