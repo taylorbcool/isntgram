4fc9c29517565319e6387d16f8aa455a55aeacac
Taylor Cool <taycool@gmail.com>
2020-06-13 20:16:20 -0700

    added register component, not wired to backend

---
+++ src/app/isntgram-client/src/components/Register.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import { 
+  Box,
+  Flex,
+  Text } from '@chakra-ui/core'
+
+  const Register = () => {
+    const [show, setShow] = useState(false);
+    const handleClick = () => setShow(!show);
+    
+    return(
+      <Box
+        maxWidth={650}
+        p={4}
+        borderRadius={4}
+        borderWidth={2}
+        borderStyle="solid"
+        m="auto"
+        my={4}
+      >
+        <Input variant="flushed" placeholder="Email" />
+        <InputGroup size="md">
+          <Input
+            pr="4.5rem"
+            type={show ? "text" : "password"}
+            placeholder="Password"
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </Box>
+    )
+  }
+
+  export default Register
